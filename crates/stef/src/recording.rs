a921c5ceedbe8e58afdc27059fa4f38dd8fd6782
Michael Dougherty <maackle.d@gmail.com>
2023-09-15 20:37:06 -0700

    WIP more generic action recorder

--- crates/stef/src/recording.rs
+++ crates/stef/src/recording.rs
@@ -1,63 +1,69 @@
 use std::{
     fs::File,
     io::{Read, Write},
+    marker::PhantomData,
     path::{Path, PathBuf},
     sync::Arc,
 };
 
-use serde::Serialize;
+use serde::{de::DeserializeOwned, Serialize};
 
 use crate::*;
 
-/// Shared access to FetchPoolState
-#[derive(Clone, Debug, derive_more::Deref)]
-pub struct RecordActions<S> {
-    #[deref]
-    state: S,
-    storage: Option<Arc<PathBuf>>,
-}
+pub trait ActionRecorder<S: State<'static>> {
+    fn initialize(&self) -> anyhow::Result<()>;
 
-impl<S> State<'static> for RecordActions<S>
-where
-    S: State<'static>,
-    S::Action: Serialize,
-{
-    type Action = S::Action;
-    type Effect = S::Effect;
+    fn record_action(&self, action: &S::Action) -> anyhow::Result<()>;
 
-    fn transition(&mut self, action: Self::Action) -> Self::Effect {
-        if let Some(path) = self.storage.as_ref() {
-            let bytes = rmp_serde::to_vec(&action).unwrap();
-            let mut f = File::options().append(true).open(&**path).unwrap();
-            let len = bytes.len() as u32;
-            f.write(&len.to_le_bytes()).unwrap();
-            f.write(&bytes).unwrap();
-        }
-        self.state.transition(action)
+    // TODO: buffer, or just provide playback_actions
+    fn retrieve_actions(&self) -> anyhow::Result<Vec<S::Action>>;
+
+    fn playback_actions(&self, state: &mut S) -> anyhow::Result<Vec<S::Effect>> {
+        Ok(self
+            .retrieve_actions()?
+            .into_iter()
+            .map(|action| state.transition(action))
+            .collect())
     }
 }
 
-impl<S> RecordActions<S>
-where
-    S: State<'static>,
-    S::Action: serde::de::DeserializeOwned,
-{
-    pub fn new(storage: Option<PathBuf>, state: S) -> Self {
-        if let Some(path) = storage.as_ref() {
-            File::options()
-                .write(true)
-                .create_new(true)
-                .open(path)
-                .expect("specified the same file twice in RecordActions");
-        }
+pub struct FileActionRecorder<S> {
+    path: PathBuf,
+    state: PhantomData<S>,
+}
+
+impl<S> From<PathBuf> for FileActionRecorder<S> {
+    fn from(path: PathBuf) -> Self {
         Self {
-            storage: storage.map(Arc::new),
-            state,
+            path,
+            state: PhantomData,
         }
     }
+}
 
-    pub fn read_actions_from_file(path: impl AsRef<Path>) -> std::io::Result<Vec<S::Action>> {
-        let mut f = File::open(path)?;
+impl<S: State<'static>> ActionRecorder<S> for FileActionRecorder<S>
+where
+    S::Action: Serialize + DeserializeOwned,
+{
+    fn initialize(&self) -> anyhow::Result<()> {
+        File::options()
+            .write(true)
+            .create_new(true)
+            .open(&self.path)?;
+        Ok(())
+    }
+
+    fn record_action(&self, action: &S::Action) -> anyhow::Result<()> {
+        let bytes = rmp_serde::to_vec(action)?;
+        let mut f = File::options().append(true).open(&self.path)?;
+        let len = bytes.len() as u32;
+        f.write(&len.to_le_bytes())?;
+        f.write(&bytes)?;
+        Ok(())
+    }
+
+    fn retrieve_actions(&self) -> anyhow::Result<Vec<S::Action>> {
+        let mut f = File::open(&self.path)?;
         let mut lbuf = [0; 4];
         let mut abuf = Vec::new();
         let mut actions = Vec::new();
@@ -70,7 +76,7 @@ where
                             return Ok(actions);
                         }
                     } else {
-                        return Err(e);
+                        return Err(e.into());
                     }
                 }
                 Ok(_) => {
@@ -84,6 +90,53 @@ where
     }
 }
 
+/// Shared access to FetchPoolState
+#[derive(Clone, Debug, derive_more::Deref)]
+pub struct RecordActions<S, R = FileActionRecorder<S>> {
+    #[deref]
+    state: S,
+    recorder: Option<Arc<R>>,
+}
+
+impl<S> State<'static> for RecordActions<S>
+where
+    S: State<'static>,
+    S::Action: Serialize + DeserializeOwned,
+{
+    type Action = S::Action;
+    type Effect = S::Effect;
+
+    fn transition(&mut self, action: Self::Action) -> Self::Effect {
+        if let Some(r) = self.recorder.as_ref() {
+            r.record_action(&action).unwrap()
+        }
+        self.state.transition(action)
+    }
+}
+
+impl<S, R> RecordActions<S, R>
+where
+    S: State<'static>,
+    S::Action: Serialize + DeserializeOwned,
+    R: ActionRecorder<S>,
+{
+    pub fn new(recorder: Option<impl Into<R>>, state: S) -> Self {
+        let recorder = recorder.map(Into::into);
+        if let Some(recorder) = recorder.as_ref() {
+            recorder.initialize().unwrap()
+        }
+        Self {
+            recorder: recorder.map(Arc::new),
+            state,
+        }
+    }
+
+    pub fn retrieve_actions(recorder: impl Into<R>) -> anyhow::Result<Vec<S::Action>> {
+        let r: R = recorder.into();
+        r.retrieve_actions()
+    }
+}
+
 #[test]
 fn action_recording_roundtrip() {
     let dir = tempfile::tempdir().unwrap();
@@ -92,6 +145,6 @@ fn action_recording_roundtrip() {
     rec.transition(());
     rec.transition(());
     rec.transition(());
-    let actions: Vec<()> = RecordActions::<()>::read_actions_from_file(&path).unwrap();
+    let actions: Vec<()> = RecordActions::<()>::retrieve_actions(path).unwrap();
     assert_eq!(actions, vec![(), (), ()]);
 }
