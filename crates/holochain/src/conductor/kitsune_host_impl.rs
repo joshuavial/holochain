0cc2dbefaffc47fdece8ccfbe9fc3646db7e682c
ThetaSinner <ThetaSinner@users.noreply.github.com>
2024-01-09 12:36:02 +0000

    Eliminate txn open when hitting p2p agent cache

--- crates/holochain/src/conductor/kitsune_host_impl.rs
+++ crates/holochain/src/conductor/kitsune_host_impl.rs
@@ -16,7 +16,7 @@ use holochain_p2p::{
     dht::{spacetime::Topology, ArqStrat},
     DnaHashExt,
 };
-use holochain_sqlite::prelude::{AsP2pMetricStoreTxExt, AsP2pStateTxExt};
+use holochain_sqlite::prelude::{AsP2pMetricStoreTxExt, AsP2pStateReadExt, AsP2pStateWriteExt};
 use holochain_types::{
     prelude::{DhtOpHash, DnaError},
     share::RwShare,
@@ -112,8 +112,7 @@ impl KitsuneHost for KitsuneHostImpl {
     ) -> KitsuneHostResult<Vec<f64>> {
         async move {
             let db = self.spaces.p2p_agents_db(&DnaHash::from_kitsune(&space))?;
-            let coverage = db
-                .read_async(move |txn| txn.p2p_extrapolated_coverage(dht_arc_set))
+            let coverage = db.p2p_extrapolated_coverage(dht_arc_set)
                 .await?;
             Ok(coverage)
         }
@@ -156,9 +155,7 @@ impl KitsuneHost for KitsuneHostImpl {
         let dna_hash = DnaHash::from_kitsune(&space);
         let db = self.spaces.p2p_agents_db(&dna_hash);
         async move {
-            Ok(db?
-                .write_async(move |txn| txn.p2p_remove_agent(&agent))
-                .await?)
+            Ok(db?.p2p_remove_agent(&agent).await?)
         }
         .boxed()
         .into()
